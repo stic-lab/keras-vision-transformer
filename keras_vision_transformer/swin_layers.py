5ef147ed6f487bba237f334a342a2b03b59b0be8
stic-lab <raannicet1@gmail.com>
2023-02-08 11:10:43 +0300

    add second shifted windows attention in the same swin transformer block

--- keras_vision_transformer/swin_layers.py
+++ keras_vision_transformer/swin_layers.py
@@ -225,6 +225,15 @@ class SwinTransformerBlock(tf.keras.layers.Layer):
             self.attn_mask = None
 
         self.built = True
+    
+
+    def get_enhanced_image(self, data, output):
+        x = data
+        for i in range(0, 3 * 24, 3):
+            r = output[:, :, :, i: i + 3]
+            x = x + r * (tf.square(x) - x)
+            
+        return x
 
     def call(self, x):
         H, W = self.num_patch
@@ -247,18 +256,28 @@ class SwinTransformerBlock(tf.keras.layers.Layer):
             shifted_x = tf.roll(x, shift=[-self.shift_size, -self.shift_size], axis=[1, 2])
         else:
             shifted_x = x
-
+        ############# FIRST WINDOWS ATTENTION ###########################
         # Window partition 
         x_windows = window_partition(shifted_x, self.window_size)
         x_windows = tf.reshape(x_windows, shape=(-1, self.window_size * self.window_size, C))
 
         # Window-based multi-headed self-attention
-        # Epoch 1/3
-        # (8192, 16, 128)
-        # (8192, 16, 128)
-        # (8192, 16, 128)
-        # (8192, 16, 128)
         attn_windows = self.attn(x_windows, mask=self.attn_mask)
+
+        ############# SECOND WINDOWS ATTENTION ###########################
+        # Merge windows
+        attn_windows = tf.reshape(attn_windows, shape=(-1, self.window_size, self.window_size, C))
+        shifted_x = window_reverse(attn_windows, self.window_size, H, W, C)
+        shifted_x = tf.roll(x, shift=[-self.shift_size, -self.shift_size], axis=[1, 2])
+        # Window partition 
+        x_windows = window_partition(shifted_x, self.window_size)
+        x_windows = tf.reshape(x_windows, shape=(-1, self.window_size * self.window_size, C))
+
+        # Window-based multi-headed self-attention
+        attn_windows = self.attn(x_windows, mask=self.attn_mask)
+        ############# END WINDOWS PARTITION ###########################
+
+        # attn_windows = self.get_enhanced_image(x_windows, attn_windows)
         # print(attn_windows.shape)
         # Epoch 1/3
         # (8192, 16, 128)
